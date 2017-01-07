653e4ab34e6eda1f319b12c158a55c6af1747343
sammarxz <sammarxzz@gmail.com>
2017-01-06 22:31:54 -0300

    Added build(/dist) pipeline with minification of stylesheets, scripts and html files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 'use strict';
 
-const gulp        = require('gulp'),
-      sass        = require('gulp-sass'),
-      nunjucks    = require('gulp-nunjucks'),
-      maps        = require('gulp-sourcemaps'),
-      concat      = require('gulp-concat'),
-      uglify      = require('gulp-uglify'),
-      rename      = require('gulp-rename');
+const gulp          = require('gulp'),
+      sass          = require('gulp-sass'),
+      nunjucks      = require('gulp-nunjucks'),
+      htmlmin       = require('gulp-htmlmin'),
+      maps          = require('gulp-sourcemaps'),
+      autoprefixer  = require("gulp-autoprefixer"),
+      concat        = require('gulp-concat'),
+      uglify        = require('gulp-uglify'),
+      cssmin        = require('gulp-cssmin'),
+      rename        = require('gulp-rename'),
+      browserSync   = require('browser-sync').create();
 
 gulp.task('concatScripts', () =>
     gulp.src('src/js/*.js')
@@ -19,16 +23,27 @@ gulp.task('concatScripts', () =>
 gulp.task('minifyScripts', () =>
     gulp.src('src/static/js/app.js')
         .pipe(uglify())
-        .pipe(rename('app.min.js'))
-        .pipe(gulp.dest('dist/static/js/'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('src/static/js/'))
 );
 
 gulp.task('compileSass', () =>
     gulp.src('src/scss/application.scss')
         .pipe(maps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(maps.write('./'))
         .pipe(gulp.dest('src/static/css'))
+);
+
+gulp.task('minifyStylesheet', () =>
+    gulp.src('src/static/css/application.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('src/static/css/'))
 );
 
 gulp.task('compileNunjucks', () =>
@@ -36,3 +51,20 @@ gulp.task('compileNunjucks', () =>
         .pipe(nunjucks.compile())
         .pipe(gulp.dest('src/'))
 );
+
+gulp.task('minifyHtml', () =>
+    gulp.src('src/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('dist'))
+);
+
+gulp.task('watch', function() {
+    gulp.watch("src/scss/**/*.scss", ['compileSass']);
+    gulp.watch("src/templates/**/*.html", ['compileNunjucks'])
+});
+
+gulp.task('build', ['minifyScripts', 'minifyStylesheet', 'minifyHtml'], function() {
+    return gulp.src(["src/static/css/application.min.css", "src/static/js/app.min.js",
+              "src/static/img/**"], { base: 'src'})
+          .pipe(gulp.dest('dist'))
+});
